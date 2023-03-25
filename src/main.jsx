a214f16f61a131a8b8f0f243fa0fe017795cc715
unfirthman <unfirthman@gmail.com>
2023-03-25 17:10:18 -0400

    second page added. blank contacts template

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
