54424943afe206839fa48ea1fb42ef150d0870a9
unfirthman <unfirthman@gmail.com>
2023-05-23 20:55:37 -0400

    routing again

--- src/navBar/Navbar.jsx
+++ src/navBar/Navbar.jsx
@@ -1,45 +1,32 @@
-
-
-import React from 'react';
-import '../routes/root/root.css'
-import './Navbar.css'
+import React from "react";
+import "../routes/root/root.css";
+import "./Navbar.css";
 
 const Navbar = () => {
-return (
-                    <nav id="nav-wrap">
-                        <ul id="nav" className="nav">
-                            <li >
-                                <a  href="/">
-                                    home
-                                </a>
-                            </li>
-                            <li>
-                                <a  href="/about">
-                                    about
-                                </a>
-                            </li>
-                            <li>
-                                <a  href="/resume">
-                                    resume
-                                </a>
-                            </li>
-                            <li>
-                                <a  href="/portfolio">
-                                    portfolio
-                                </a>
-                            </li>
-                            <li>
-                                <a  href="/contact">
-                                    contact
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/blog">blog</a>
-                            </li>
-                        </ul>
-                    </nav>
-);
+    return (
+        <nav id="nav-wrap">
+            <ul id="nav" className="nav">
+                <li>
+                    <a href="/">home</a>
+                </li>
+                <li>
+                    <a href="/michael-hanf-portfolio/about">about</a>
+                </li>
+                <li>
+                    <a href="/michael-hanf-portfolio/resume">resume</a>
+                </li>
+                <li>
+                    <a href="/michael-hanf-portfolio/portfolio">portfolio</a>
+                </li>
+                <li>
+                    <a href="/michael-hanf-portfolio/contact">contact</a>
+                </li>
+                <li>
+                    <a href="/michael-hanf-portfolio/blog">blog</a>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
-
