d925907cbb5d3da300f82dfa051f3e02a6251836
unfirthman <unfirthman@gmail.com>
2023-05-30 14:31:39 -0400

    new config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.md'],
-    base: "/michael-hanf-portfolio/"
+    base: "/michael-hanf-portfolio/",
+    resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+},
 
 })
